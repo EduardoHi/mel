f72703930c0291e9a0e3dea0e7418303ef4ca7c2
unknown <eshidalgo97@gmail.com>
2017-12-25 23:09:10 -0600

    switch to canvas

--- mel.js
+++ mel.js
@@ -1,63 +1,50 @@
-var svgNS = "http://www.w3.org/2000/svg";
-var w_width = window.innerWidth
-var w_height = window.innerHeight
-var center = [w_width / 2, w_height / 2];
-
 rand = (n) => Math.random() * n;
 sin = (n) => Math.sin(n);
 cos = (n) => Math.cos(n);
 
 var a = 0;
 var r1 = 50;
-var r2 = r1 + 30;
+var r2 = 30;
+var rp2 = 110;
 
 function Mel() {
-    createCircle(center[0], center[1], r1);
-    var secondCircle = createCircle(center[0], 110 + center[1], 30);
-    var lineElement = createLine(center[0] + r1, center[1] + r1, center[0] + r2, center[1] + r2);
+    var cnvs = document.createElement("canvas");
+
+    var width = Math.floor(window.innerWidth), height = Math.floor(window.innerHeight);
+
+    cnvs.setAttribute("width", width);
+    cnvs.setAttribute("height", height);
+    document.body.appendChild(cnvs);
+
+    var ctx = cnvs.getContext("2d");
+    ctx.webkitImageSmoothingEnabled = true;
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 7;
+    ctx.translate(width / 2, height / 2);
+
 
     this.update = () => {
+        ctx.clearRect(-width / 2, -height / 2, width, height);
+        circle(0, 0, r1);
+        circle(rp2 * sin(a), rp2 * cos(a), r2);
+        line(r1 * sin(a), r1 * cos(a), (rp2 - r2) * sin(a), (rp2 - r2) * cos(a));
         a += 0.01;
-        updateLine(lineElement);
-        updateCircle(secondCircle);
     }
 
     this._intervalId = setInterval(this.update, 1000 / 60);
+
+
+    function line(x1, y1, x2, y2) {
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.stroke();
+    }
+
+    function circle(x, y, r) {
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
 }
 
-updateLine = (line) => {
-    line.setAttributeNS(null, "x1", center[0] + sin(a) * r1);
-    line.setAttributeNS(null, "y1", center[1] + cos(a) * r1);
-    line.setAttributeNS(null, "x2", center[0] + sin(a) * r2);
-    line.setAttributeNS(null, "y2", center[1] + cos(a) * r2);
-}
-
-updateCircle = (circle) => {
-    circle.setAttributeNS(null, "cx", center[0] + sin(a) * (r2 + 30));
-    circle.setAttributeNS(null, "cy", center[1] + cos(a) * (r2 + 30));
-}
-
-function createCircle(x, y, r) {
-    var circ = document.createElementNS(svgNS, "circle");
-    circ.setAttributeNS(null, "cx", x);
-    circ.setAttributeNS(null, "cy", y);
-    circ.setAttributeNS(null, "r", r);
-    circ.setAttributeNS(null, "stroke", "white");
-    circ.setAttributeNS(null, "stroke-width", "5");
-    circ.setAttributeNS(null, "fill-opacity", "0.0");
-
-    document.getElementById("mySVG").appendChild(circ);
-    return circ;
-}
-function createLine(x1, y1, x2, y2) {
-    var line = document.createElementNS(svgNS, "line");
-    line.setAttributeNS(null, "x1", x1);
-    line.setAttributeNS(null, "y1", y1);
-    line.setAttributeNS(null, "x2", x2);
-    line.setAttributeNS(null, "y2", y2);
-    line.setAttributeNS(null, "stroke", "white");
-    line.setAttributeNS(null, "stroke-width", "5");
-
-    document.getElementById("mySVG").appendChild(line);
-    return line;
-}
